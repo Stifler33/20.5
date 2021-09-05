c42487a9a574dd1f072ee79bdc8c4e4896f603ce
stifler33 <bagi33.ru@gmail.com>
2021-09-05 20:36:08 +0300

    task 1 finish

--- task1.cpp
+++ task1.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
-
+#include <fstream>
+void add_in_data(std::ofstream &data ,char sy = ' ',std::string textUser = ""){
+    getline(std::cin, textUser);
+    data << textUser << sy;
+}
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::ofstream data("D:\\register.txt");
+    std::string stopWord;
+    while (stopWord != "n"){
+        std::cout << "Enter full name :";
+        add_in_data(data);
+        std::cout << "Enter date :";
+        add_in_data(data);
+        std::cout << "Enter sum salary :";
+        add_in_data(data, '\n');
+        std::cout << "proceed ?\n";
+        getline(std::cin , stopWord);
+    }
+    data.close();
     return 0;
 }
