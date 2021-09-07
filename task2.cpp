5a799aab76c05a4534ff4167b9f6b2e51c5c6d42
stifler33 <bagi33.ru@gmail.com>
2021-09-07 21:58:53 +0300

    task 2 finish

--- task2.cpp
+++ task2.cpp
@@ -1,4 +1,25 @@
-//
-// Created by bagi3 on 04.09.2021.
-//
-
+#include <iostream>
+#include <fstream>
+#include <ctime>
+using namespace std;
+int getRandNumb(){
+    double x = 1.0 / RAND_MAX;
+    return int(rand() * x * 2);
+}
+int main(){
+    ofstream outDrawing ("D:\\Drawing.txt", ios_base::app);
+    srand(time(nullptr));
+    int W;
+    int H;
+    cout << "Width and Height\n";
+    cin >> W >> H;
+    for (int h = 0; h < H; h++){
+        for (int w = 0; w < W; w++){
+            outDrawing << getRandNumb() << " ";
+        }
+        outDrawing << '\n';
+    }
+    outDrawing << "*************************\n";
+    outDrawing.close();
+    return 0;
+}
