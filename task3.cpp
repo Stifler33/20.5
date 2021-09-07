9a19ae5651a7378cf8be418c0d0d250fc493ea2c
stifler33 <bagi33.ru@gmail.com>
2021-09-07 22:41:18 +0300

    task 3 init

--- task3.cpp
+++ task3.cpp
@@ -1,4 +1,26 @@
-//
-// Created by bagi3 on 04.09.2021.
-//
+#include <iostream>
+#include <fstream>
 
+using namespace std;
+int main(){
+    ifstream river("D:\\river.txt");
+    ofstream basket("D:\\basket.txt", ios_base::app);
+    string fish;
+    cout << "Enter fish:\n";
+    cin >> fish;
+    if (!river.is_open()) {
+        cerr << "Error open file !";
+        return 0;
+    }
+    int countFishing = 0;
+    while (!river.eof()){
+        string getFish;
+        river >> getFish;
+        if (getFish == fish) {
+            basket << fish << '\n';
+            countFishing++;
+        }
+    }
+    cout << countFishing;
+    return 0;
+}
