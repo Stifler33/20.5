48e2b9333f3d948b51f33079fa5af51f6c16a847
stifler <bagi33.ru@gmail.com>
2021-09-09 17:05:01 +0300

    task 3 modified office

--- task3.cpp
+++ task3.cpp
@@ -6,12 +6,12 @@ int main(){
     ifstream river("D:\\river.txt");
     ofstream basket("D:\\basket.txt", ios_base::app);
     string fish;
-    cout << "Enter fish:\n";
-    cin >> fish;
     if (!river.is_open()) {
         cerr << "Error open file !";
         return 0;
     }
+    cout << "Enter fish:\n";
+    cin >> fish;
     int countFishing = 0;
     while (!river.eof()){
         string getFish;
