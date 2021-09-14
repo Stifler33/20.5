15d260ed6ae614942f4ae9b51e502784dba2c9bf
stifler33 <bagi33.ru@gmail.com>
2021-09-14 21:58:11 +0300

    task 4 added money in bank

--- task4.cpp
+++ task4.cpp
@@ -2,44 +2,49 @@
 #include <fstream>
 #include <cstdlib>
 #include <ctime>
+#include <cstring>
 using namespace std;
+int countMoney;
 int getRandNumb(){
     double x = 1.0 / RAND_MAX;
     return int(rand() * x * 6 + 1);
 }
-void addMoney(ofstream &atm, ifstream &getAtm){
-    int countMoney = 0;
-    getAtm >> countMoney;
-    for (int i = 10 - countMoney; i > 0; i--) {
+void addMoney(){
+    fstream atm("D:\\ATM\\money.bin", ios_base::binary | ios::in | ios::out | ios::ate);
+    atm.is_open() ? cout << "file open !\n" : cout << "Error open file !\n";
+    int endPos = atm.tellp();
+    atm.seekg(0, ios::beg);
+    atm >> countMoney;
+    atm.seekp(endPos, ios::beg);
+    for (int i = 1000 - countMoney; i > 0; i--) {
         countMoney++;
         switch (getRandNumb()) {
             case 1:
-                atm << 100 << '\n';
+                atm << "100" << '\n';
                 break;
             case 2:
-                atm << 200 << '\n';
+                atm << "200" << '\n';
                 break;
             case 3:
-                atm << 500 << '\n';
+                atm << "500" << '\n';
                 break;
             case 4:
-                atm << 1000 << '\n';
+                atm << "1000" << '\n';
                 break;
             case 5:
-                atm << 2000 << '\n';
+                atm << "2000" << '\n';
                 break;
             case 6:
-                atm << 5000 << '\n';
+                atm << "5000" << '\n';
                 break;
         }
     }
-    atm.seekp(2, ios_base::beg);
+    atm.seekp(0, ios::beg);
     atm << countMoney;
+    atm.close();
 }
 int main() {
-    ofstream atm ("D:\\ATM\\money.txt", ios_base::binary | ios_base::app);
-    ifstream getAtm ("D:\\ATM\\money.txt");
     getRandNumb();
-    addMoney(atm, getAtm);
+    addMoney();
     return 0;
 }
