d454bd6173e124dbd3d67a350c0a07d670e05637
stifler33 <bagi33.ru@gmail.com>
2021-09-16 20:37:31 +0300

    task 4 added init file, added money

--- task4.cpp
+++ task4.cpp
@@ -9,42 +9,66 @@ int getRandNumb(){
     double x = 1.0 / RAND_MAX;
     return int(rand() * x * 6 + 1);
 }
+void initFile(int countStr = 1000){
+    countStr += 2;
+    fstream init("D:\\ATM\\money.bin", ios::binary | ios::trunc | ios::out);
+    for (int i = 0; i < countStr; i++){
+        if (i == 0) {
+            init << "c0   \n";
+        }else if (i < countStr - 1){
+            init << "m    \n";
+        }else init << "q";
+    }
+    init.close();
+    cout << "File initialized !\n";
+}
 void addMoney(){
-    fstream atm("D:\\ATM\\money.bin", ios_base::binary | ios::in | ios::out | ios::ate);
+    fstream atm("D:\\ATM\\money.bin", ios_base::binary | ios::in | ios::out);
     atm.is_open() ? cout << "file open !\n" : cout << "Error open file !\n";
-    int endPos = atm.tellp();
-    atm.seekg(0, ios::beg);
+    atm.seekg(1, ios::beg);
     atm >> countMoney;
-    atm.seekp(endPos, ios::beg);
-    for (int i = 1000 - countMoney; i > 0; i--) {
-        countMoney++;
-        switch (getRandNumb()) {
-            case 1:
-                atm << "100" << '\n';
-                break;
-            case 2:
-                atm << "200" << '\n';
-                break;
-            case 3:
-                atm << "500" << '\n';
-                break;
-            case 4:
-                atm << "1000" << '\n';
-                break;
-            case 5:
-                atm << "2000" << '\n';
-                break;
-            case 6:
-                atm << "5000" << '\n';
-                break;
+    char sy;
+    while (sy != 'q') {
+        atm.read(&sy, 1);
+        //atm.seekp(-1, ios::cur);
+        if (sy == 'm') {
+            countMoney++;
+            switch (getRandNumb()) {
+                case 1:
+                    atm << "100 ";
+                    break;
+                case 2:
+                    atm << "200 ";
+                    break;
+                case 3:
+                    atm << "500 ";
+                    break;
+                case 4:
+                    atm << "1000";
+                    break;
+                case 5:
+                    atm << "2000";
+                    break;
+                case 6:
+                    atm << "5000";
+                    break;
+            }
         }
     }
-    atm.seekp(0, ios::beg);
+    atm.seekp(1, ios::beg);
     atm << countMoney;
     atm.close();
 }
 int main() {
     getRandNumb();
-    addMoney();
+    string userAns;
+    cout << "enter action:\n";
+    cin >> userAns;
+    if (userAns == "init"){
+        initFile(10);
+    }
+    if (userAns == "add"){
+        addMoney();
+    }
     return 0;
 }
